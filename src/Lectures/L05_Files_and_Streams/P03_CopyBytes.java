7dac4463444f57561e41dbffaae4dffe7b2c2933
Angel Dzhambazov <angel.dzhambazov@gmail.com>
2018-06-06 18:14:08 +0300

    someFiles

--- src/Lectures/L05_Files_and_Streams/P03_CopyBytes.java
+++ src/Lectures/L05_Files_and_Streams/P03_CopyBytes.java
@@ -4,37 +4,34 @@ package Lectures.L05_Files_and_Streams;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.StreamCorruptedException;
 
-public class P02_WriteToFile {
+public class P03_CopyBytes {
     private static String PATH = "G:\\github june 2018\\JavaAdvanced\\src\\Lectures\\L05_Files_and_Streams\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+
     private static String OUTPUT_PATH = "G:\\github june 2018\\JavaAdvanced\\src\\Lectures\\L05_Files_and_Streams\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\outputP02.txt";
 
     public static void main(String[] args) {
-        String pattern = "[,\\.\\!\\?]";
 
-        List<Character> notNeededCharacters = new ArrayList<>();
-        notNeededCharacters.add(',');
-        notNeededCharacters.add('.');
-        notNeededCharacters.add('!');
-        notNeededCharacters.add('?');
 
         try (FileInputStream fileInputStream = new FileInputStream(PATH);
              FileOutputStream fileOutputStream = new FileOutputStream(OUTPUT_PATH)) {
             int oneByte = fileInputStream.read();
             while (oneByte >= 0) {
-                if (!notNeededCharacters.contains((char) oneByte)) {
-                    fileOutputStream.write(oneByte);
+                String digits = String.valueOf(oneByte);
+
+                for (int i = 0; i < digits.length(); i++) {
+                    if (digits.charAt(i) == 32) {
+                        fileOutputStream.write(Integer.parseInt(String.valueOf(' ')));
+                    } else {
+                        fileOutputStream.write(digits.charAt(i));
+                    }
                 }
                 oneByte = fileInputStream.read();
             }
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
+
     }
-
 }
-
-
-//Skip the following symbols: ',', '.', '!', '?'
