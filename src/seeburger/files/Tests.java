69e4c4031af078af9f7daa15cecaf4c33ed28811
Angel Dzhambazov <angel.dzhambazov@gmail.com>
2018-06-19 00:47:03 +0300

    hmmm

--- src/seeburger/files/Tests.java
+++ src/seeburger/files/Tests.java
@@ -1,21 +1,48 @@
 package seeburger.files;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Tests {
 
-	public static void main(String[] args) {
-		File whatDoYouHold = new File("C:\\Users\\a.dzhambazov\\Desktop\\deklaracii");
-		System.out.println(whatDoYouHold.isDirectory());
-		System.out.println(whatDoYouHold.isFile());
-		System.out.println(whatDoYouHold.isHidden());
-		System.out.println();
-		for (File file : whatDoYouHold.listFiles()) {
-			if (file.isFile()) {
-				System.out.println(file.getName());
+
+	public static void main(String[] args) throws IOException {
+		 int[] x = {1};
+
+		Runnable task = () -> {
+			for (int i = 0; i < 100; i++) {
+				x[0]++;
+
 			}
+			System.out.println(x[0]);
+		};
+
+		Thread moqtaNishka = new Thread(task);
+		moqtaNishka.setDaemon(false);
+		moqtaNishka.start();
+
+
+
+
+		Map<String, String> testMap = new HashMap<>();
+		testMap.put("Pesho", "Slab 2");
+		testMap.put("Pesho", "prezapisvash li e?");
+		for (Map.Entry<String, String> stringStringEntry : testMap.entrySet()) {
+			System.out.println(stringStringEntry.getKey());
+			System.out.println(stringStringEntry.getValue());
 		}
+
+		String xxxxxxx = "b";
+		String xxxx = "c";
+
+		System.out.println(xxxx.compareTo(xxxxxxx));
+
 	}
+
+
 }
 
 
